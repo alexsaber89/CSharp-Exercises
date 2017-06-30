b3b8f86759da7fcfb6ca5e51d15952576e6adf12
Alex Saber <alexsaber89@gmail.com>
2017-06-30 11:30:13 -0500

    all tests passing

--- CSExercises.Attempts/Exercises/Bob.cs
+++ CSExercises.Attempts/Exercises/Bob.cs
@@ -8,9 +8,55 @@ namespace CSExercises.Attempts.Exercises
 {
     public static class Bob
     {
+        //http://exercism.io/exercises/csharp/bob/readme
+
+        public static bool IsAllUpper(char[] input)
+        {
+            if (input.Length < 1)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsLetter(input[i]) && !Char.IsUpper(input[i]))
+                    return false;
+            }
+
+            return true;
+        }
+
         public static string Response(string input)
         {
-            throw new NotImplementedException();
+            string noSpaceInput = input.Replace(" ", "");
+
+            // Ensure noSpaceInput is not null
+            if (!string.IsNullOrWhiteSpace(noSpaceInput))
+            {
+                // Extract into array of characters
+                char[] inputCharArray = noSpaceInput.ToCharArray();
+
+                var cleanCharArray = noSpaceInput.Where(Char.IsLetter).ToArray();
+
+                // Check for all caps
+                // If all caps, return "Whoa, chill out!"
+                if (IsAllUpper(cleanCharArray))
+                {
+                    return "Whoa, chill out!";
+                }
+
+
+                // Check last character for punctuation type
+                if (inputCharArray[inputCharArray.Length - 1].ToString() == "?")
+                {
+                    return "Sure.";
+                }
+
+                return "Whatever.";
+            }
+
+            // If null, return "Fine. Be that way!"
+            return "Fine. Be that way!";
         }
     }
 }
