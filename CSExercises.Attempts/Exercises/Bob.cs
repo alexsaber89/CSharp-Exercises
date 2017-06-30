776a5fb93a5919c7eb461bdd6507515e7e3b0cc3
Alex Saber <alexsaber89@gmail.com>
2017-06-30 09:46:59 -0500

    set up solution;  add files for Bob exerise

---
+++ CSExercises.Attempts/Exercises/Bob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSExercises.Attempts.Exercises
+{
+    public static class Bob
+    {
+        public static string Response(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
