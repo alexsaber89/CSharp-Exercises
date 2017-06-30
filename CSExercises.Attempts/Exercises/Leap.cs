6665b2b121a80279205744c3b5dea2432e064b0e
Alex Saber <alexsaber89@gmail.com>
2017-06-30 11:49:22 -0500

    add exercise files

---
+++ CSExercises.Attempts/Exercises/Leap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSExercises.Attempts.Exercises
+{
+    public static class Leap
+    {
+        public static bool IsLeapYear(int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSExercises.Tests/Tests/LeapTests.cs
@@ -0,0 +1,33 @@
+﻿using CSExercises.Attempts.Exercises;
+using System;
+using Xunit;
+
+namespace CSExercises.Tests.Tests
+{
+    public class LeapTests
+    {
+        [Fact]
+        public void Year_not_divisible_by_4_is_common_year()
+        {
+            Assert.False(Leap.IsLeapYear(2015));
+        }
+
+        [Fact]
+        public void Year_divisible_by_4_not_divisible_by_100_is_leap_year()
+        {
+            Assert.True(Leap.IsLeapYear(2016));
+        }
+
+        [Fact]
+        public void Year_divisible_by_100_not_divisible_by_400_is_common_year()
+        {
+            Assert.False(Leap.IsLeapYear(2100));
+        }
+
+        [Fact]
+        public void Year_divisible_by_400_is_leap_year()
+        {
+            Assert.True(Leap.IsLeapYear(2000));
+        }
+    }
+}
