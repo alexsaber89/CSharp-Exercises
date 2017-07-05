cef657dc2b679955131049a3aad73d2f45745e78
Alex Saber <alexsaber89@gmail.com>
2017-07-05 15:17:19 -0500

    leap unit tests passing

--- CSExercises.Attempts/Exercises/Leap.cs
+++ CSExercises.Attempts/Exercises/Leap.cs
@@ -8,9 +8,23 @@ namespace CSExercises.Attempts.Exercises
 {
     public static class Leap
     {
+        //http://exercism.io/exercises/csharp/leap/readme
+        //on every year that is evenly divisible by 4
+        //except every year that is evenly divisible by 100
+        //unless the year is also evenly divisible by 400
+
         public static bool IsLeapYear(int year)
         {
-            throw new NotImplementedException();
+            if (year % 4 == 0)
+            {
+                if (year % 400 != 0 && year % 100 == 0)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            return false;
         }
     }
 }
