d01b05e9a3e2d6659a9131806f81729ba36ac9af
Alex Saber <alexsaber89@gmail.com>
2017-07-06 10:31:03 -0500

    re-order arguments for readability; comment code

--- CSExercises.Attempts/Exercises/Leap.cs
+++ CSExercises.Attempts/Exercises/Leap.cs
@@ -9,15 +9,15 @@ namespace CSExercises.Attempts.Exercises
     public static class Leap
     {
         //http://exercism.io/exercises/csharp/leap/readme
-        //on every year that is evenly divisible by 4
-        //except every year that is evenly divisible by 100
-        //unless the year is also evenly divisible by 400
 
         public static bool IsLeapYear(int year)
         {
+            //on every year that is evenly divisible by 4
             if (year % 4 == 0)
             {
-                if (year % 400 != 0 && year % 100 == 0)
+                //except every year that is evenly divisible by 100
+                //unless the year is also evenly divisible by 400
+                if (year % 100 == 0 && year % 400 != 0)
                 {
                     return false;
                 }
